62c5d712eeeb24ab528c19950a32ca2ba149457c
pavlo_sadivnychyi <pavlo.sadivnychyi@devlight.io>
2025-07-17 16:15:00 +0300

    Initial backend setup

---
+++ alembic/versions/001_initial_migration.py
@@ -0,0 +1,27 @@
+"""Initial migration
+
+Revision ID: 001
+Revises: 
+Create Date: 2024-01-01 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '001'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Create all tables manually
+    # This is a placeholder - Railway will create tables using init_db.py
+    pass
+
+
+def downgrade():
+    # Drop all tables
+    pass
